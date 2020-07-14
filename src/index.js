a050bc4b331f4b20a878bf693d160b0ff4551b1d
ali <ali.marinas@icloud.com>
2020-07-14 15:03:55 -0700

    added context

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDom from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 
+import { ContextProvider } from "./Context";
 import App from "./App";
 
 ReactDom.render(
-  <Router>
-    <App />
-  </Router>,
-
+  <ContextProvider>
+    <Router>
+      <App />
+    </Router>
+    ,
+  </ContextProvider>,
   document.getElementById("root")
 );
