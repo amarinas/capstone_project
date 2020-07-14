3567bde4af9e6a7db11f1a7621f1347bdb95ded0
ali <ali.marinas@icloud.com>
2020-07-13 18:07:56 -0700

    initial commit edited to get the base started

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h2>Pic Some</h2>
+      <i classname="ri-shopping-cart-line ri-fw ri-2x"></i>
+    </header>
+  );
+}
+
+export default Header;
