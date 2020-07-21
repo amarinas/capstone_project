c2f467aac64d5179578a50cac4efbfda77b40b28
ali <ali.marinas@icloud.com>
2020-07-21 15:44:38 -0700

    set up the cart page with adding and removing items

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { Context } from "../Context";
 
 function Header() {
+  const { cartItems } = useContext(Context);
+  const cartClassName =
+    cartItems.length > 0
+      ? "ri-shopping-cart-fill ri-3x"
+      : "ri-shopping-cart-line ri-2x";
+
   return (
     <header>
       <h2>
         <Link to="/">Pic Some</Link>
       </h2>
       <Link to="/cart">
-        <i className="ri-shopping-cart-2-fill ri-3x"></i>
+        <i className={cartClassName}></i>
       </Link>
     </header>
   );
--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Context } from "../Context";
+import CartItem from "../components/CartItem";
 
 function Cart() {
+  const { cartItems } = useContext(Context);
+  const cartItemsElements = cartItems.map((item) => (
+    <CartItem key={item.id} item={item} />
+  ));
   return (
     <main className="cart-page">
       <h1> Check Out </h1>
+      {cartItemsElements}
+      <p className="total-cost">Total: </p>
+      <div className="order-button">
+        <button>Place Order</button>
+      </div>
     </main>
   );
 }
